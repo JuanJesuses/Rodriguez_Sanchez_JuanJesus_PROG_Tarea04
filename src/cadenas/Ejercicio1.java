23ecc5132402af4b57e88d5841e691c391407f46
john <john@john-X555LAB>
2017-11-22 22:05:23 +0100

    Se crea el paquete cadenas y la clase Ejercicio1 con el método main.

---
+++ src/cadenas/Ejercicio1.java
@@ -0,0 +1,59 @@
+package cadenas;
+
+import utilidades.Entrada;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		String palabra;
+		char continua;
+		int contadorA=0;
+		int contadorE=0;
+		int contadorI=0;
+		int contadorO=0;
+		int contadorU=0;
+		
+		do {
+			System.out.println("Introduce una palabra para saber si es panvocálica o no: ");
+			palabra = Entrada.cadena();
+			
+			for(int i=0; i<palabra.length(); i++) {
+				switch(palabra.charAt(i)) {
+					case 'a':
+						contadorA++;
+						break;
+					case 'e':
+						contadorE++;
+						break;
+					case 'i':
+						contadorI++;
+						break;
+					case 'o':
+						contadorO++;
+						break;
+					case 'u':
+						contadorU++;
+				}
+			}
+			
+			if(contadorA>0 && contadorE>0 && contadorI>0 && contadorO>0 && contadorU>0) {
+				System.out.printf("La palabra %s es panvocálica.\n", palabra);
+			}else {
+				System.out.printf("La palabra %s no es panvocálica.\n", palabra);
+			}
+			
+			System.out.println("¿Quieres probar con otra palabra? (s/n)");
+			continua = Entrada.caracter();
+			
+			contadorA=0;
+			contadorE=0;
+			contadorI=0;
+			contadorO=0;
+			contadorU=0;
+			
+		}while(continua == 's');
+
+	}
+
+}
