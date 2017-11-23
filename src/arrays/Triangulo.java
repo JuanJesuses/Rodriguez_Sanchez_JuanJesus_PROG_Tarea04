afd79685a9a59a11ae5564395d6f9665bc5ef13a
john <john@john-X555LAB>
2017-11-23 17:27:45 +0100

    Se crea el constructor de la clase Triángulo.

--- src/arrays/Triangulo.java
+++ src/arrays/Triangulo.java
@@ -5,6 +5,12 @@ public class Triangulo {
 	private Punto puntoA;
 	private Punto puntoB;
 	private Punto puntoC;
+	
+	public Triangulo(Punto puntoA, Punto puntoB, Punto puntoC) {
+		this.puntoA = new Punto (puntoA.getX(), puntoA.getY());
+		this.puntoB = new Punto (puntoB.getX(), puntoB.getY());
+		this.puntoC = new Punto (puntoC.getX(), puntoC.getY());
+	}
 
 
 }
