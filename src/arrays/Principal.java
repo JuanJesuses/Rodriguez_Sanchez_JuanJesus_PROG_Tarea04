0c93ccb64474ed49009919d8d64001053b1ce351
john <john@john-X555LAB>
2017-11-24 17:03:14 +0100

    Se crea el array de triángulos del tamaño pedido al usuario.

--- src/arrays/Principal.java
+++ src/arrays/Principal.java
@@ -1,10 +1,18 @@
 package arrays;
 
+import utilidades.Entrada;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
-
+		int numeroTriangulos;
+		Triangulo[] arrayDeTriangulos;
+		
+		System.out.println("Introduce el número de triágulos que quieres crear: ");
+		numeroTriangulos = Entrada.entero();
+		arrayDeTriangulos = new Triangulo[numeroTriangulos];
+		
 	}
 
 }
