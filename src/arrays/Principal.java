668da4547ad9451e4fafd69e0e43d1bea15dae5a
john <john@john-X555LAB>
2017-11-25 23:26:09 +0100

    Se crean los vértices aleatorios y se rellena el array de triángulos con esos vértices.

--- src/arrays/Principal.java
+++ src/arrays/Principal.java
@@ -1,6 +1,7 @@
 package arrays;
 
 import utilidades.Entrada;
+import java.util.Random;
 
 public class Principal {
 
@@ -9,9 +10,18 @@ public class Principal {
 		int numeroTriangulos;
 		Triangulo[] arrayDeTriangulos;
 		
+		
 		System.out.println("Introduce el número de triágulos que quieres crear: ");
 		numeroTriangulos = Entrada.entero();
 		arrayDeTriangulos = new Triangulo[numeroTriangulos];
+		
+		
+		for(int i=0; i<arrayDeTriangulos.length-1; i++) {
+			Punto verticeA = new Punto((int) Math.random()*100, (int) Math.random()*100);
+			Punto verticeB = new Punto((int) Math.random()*100, (int) Math.random()*100);
+			Punto verticeC = new Punto((int) Math.random()*100, (int) Math.random()*100);
+			arrayDeTriangulos[i]= new Triangulo (verticeA, verticeB, verticeC);
+		}
 		
 	}
 
